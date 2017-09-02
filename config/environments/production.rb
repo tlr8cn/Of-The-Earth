92295ed80a47eb08dfc8946d16b06e33c7b91303
tlr8cn <tlr8cn@virginia.edu>
2017-09-02 15:10:40 -0400

    changed mailer settings for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,8 +66,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :user_name            => ENV['gmail_username'],
-   :password             => ENV['gmail_password'],
+   :user_name            => "stphotobot@gmail.com",
+   :password             => "godofwar38",
    :authentication       => "plain",
   :enable_starttls_auto => true
   }
